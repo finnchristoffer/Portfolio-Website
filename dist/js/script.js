989bc939ac93be04e69f4897da44a9ea47702d02
finnchristoffer <finn.christoffer@gmail.com>
2023-02-16 22:55:35 +0700

    Adding Dark Mode

--- dist/js/script.js
+++ dist/js/script.js
@@ -39,7 +39,18 @@ const html = document.querySelector('html');
 darkToggle.addEventListener('click', function () {
     if (darkToggle.checked) {
         html.classList.add('dark');
+        localStorage.theme = 'dark';
     } else {
         html.classList.remove('dark');
+        localStorage.theme = 'light';
     }
 });
+
+
+
+// switch toggle position base on mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true;
+  } else {
+    darkToggle.checked = false;
+  }
